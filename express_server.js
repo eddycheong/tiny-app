2258fd7cf418afe8d5fec5ec6595c5b9ce4e68e2
eddycheong <cheong.e49@gmail.com>
2017-10-10 21:17:43 +0000

    Implemented generateRandomString function to return a 6-length random alphanumeric string

--- express_server.js
+++ express_server.js
@@ -1,3 +1,27 @@
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function generateRandomString() {
+  const numberSet = "0123456789";
+  const characterSet = "abcdefghijklmnopqrstuvwxyz";
+
+  const validCharacters = numberSet
+    + characterSet
+    + characterSet.toUpperCase();
+
+  let generatedString = "";
+
+  for(let i = 0; i < 6; i++) {
+    generatedString += validCharacters[getRandomInt(0, validCharacters.length)];
+  }
+
+  return generatedString;
+}
+
+
 const bodyParser = require("body-parser");
 var express = require("express");
 var app = express();

1edf0e7c3d7398c8be467dab77002eba291bad95
eddycheong <cheong.e49@gmail.com>
2017-10-11 22:57:05 +0000

    Refactored all routes to be handled by router.js

--- express_server.js
+++ express_server.js
@@ -1,119 +1,16 @@
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
-
-const users = {
-  "userRandomID": {
-    id: "userRandomID",
-    email: "user@example.com",
-    password: "purple-monkey-dinosaur"
-  },
- "user2RandomID": {
-    id: "user2RandomID",
-    email: "user2@example.com",
-    password: "dishwasher-funk"
-  }
-};
-
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-const urlsRouter = require("./routes/urls_router.js");
-
-const generateRandomString = require("./lib/generate_random_string");
+const router = require("./routes/router.js");
 
 app.set("view engine", "ejs");
-app.set("urls", urlDatabase);
-app.set("users", users);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
-app.use('/urls', urlsRouter);
-
-app.get("/", (req, res) => {
-  res.end("Hello!");
-});
-
-app.get("/register", (req, res) => {
-  res.render('register');
-});
-
-app.post("/register", (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-
-  if(!email || !password) {
-    res.status(400).send("Could not register a new user. Either email or password is empty.");
-  }
-
-  for(const user in users) {
-    if(email === users[user].email) {
-      res.status(400).send("A user is already registered with that email");
-      return;
-    }
-  }
-
-  const newUserId = generateRandomString();
-  users[newUserId] = {
-    id: newUserId,
-    email: email,
-    password: password
-  };
-  res.cookie("user_id", newUserId);
-
-  res.redirect("/urls");
-});
-
-app.get("/u/:shortURL", (req, res) => {
-  // TODO: refactor the validation by using app.params or router.params
-  if(!urlDatabase[req.params.shortURL]) {
-    res.status(404).send(`Could not find the short URL: ${req.params.shortURL}`);
-  }
-  else {
-    let longURL = urlDatabase[req.params.shortURL];
-    res.redirect(longURL);
-  }
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/login", (req, res) => {
-  res.render('login');
-});
-
-app.post("/login", (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-
-  console.log(email, password);
-
-  for(const userID in users) {
-    const user = users[userID];
-    if(user.email === email) {
-      if(user.password == password) {
-        res.cookie("user_id", user.id);
-        res.redirect('/');
-      } else {
-        res.status(403).send("The password for this email is incorrect.");
-      }
-
-      return;
-    }
-  }
-
-  res.status(403).send("The email user was not found.");
-});
-
-app.post("/logout", (req, res) => {
-  res.clearCookie("user_id");
-  res.redirect('/urls');
-});
+app.use('/', router);
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
--- routes/router.js
+++ routes/router.js
@@ -1,15 +1,117 @@
-var generateRandomString = require('../lib/generate_random_string');
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
 
-var express = require('express');
-var router = express.Router();
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
+const generateRandomString = require('../lib/generate_random_string');
+
+const express = require('express');
+const router = express.Router();
 
 router.use(function (req, res, next) {
   next();
 });
 
-router.get("/new", (req, res) => {
+router.get("/", (req, res) => {
+  res.end("Hello!");
+});
+
+router.get("/register", (req, res) => {
+  res.render('register');
+});
+
+router.post("/register", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if(!email || !password) {
+    res.status(400).send("Could not register a new user. Either email or password is empty.");
+  }
+
+  for(const user in users) {
+    if(email === users[user].email) {
+      res.status(400).send("A user is already registered with that email");
+      return;
+    }
+  }
+
+  const newUserId = generateRandomString();
+  users[newUserId] = {
+    id: newUserId,
+    email: email,
+    password: password
+  };
+  res.cookie("user_id", newUserId);
+
+  res.redirect("/urls");
+});
+
+router.get("/u/:shortURL", (req, res) => {
+  // TODO: refactor the validation by using router.params or router.params
+  if(!urlDatabase[req.params.shortURL]) {
+    res.status(404).send(`Could not find the short URL: ${req.params.shortURL}`);
+  }
+  else {
+    let longURL = urlDatabase[req.params.shortURL];
+    res.redirect(longURL);
+  }
+});
+
+router.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+router.get("/login", (req, res) => {
+  res.render('login');
+});
+
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  console.log(email, password);
+
+  for(const userID in users) {
+    const user = users[userID];
+    if(user.email === email) {
+      if(user.password == password) {
+        res.cookie("user_id", user.id);
+        res.redirect('/');
+      } else {
+        res.status(403).send("The password for this email is incorrect.");
+      }
+
+      return;
+    }
+  }
+
+  res.status(403).send("The email user was not found.");
+});
+
+router.post("/logout", (req, res) => {
+  res.clearCookie("user_id");
+  res.redirect('/urls');
+});
+
+/* -----------------------------------------
+  ROUTE: /urls
+ ----------------------------------------- */
+router.get("/urls/new", (req, res) => {
   const user_id = req.cookies.user_id;
-  const user = req.app.get('users')[user_id];
+  const user = users[user_id];
 
   let templateVars = {
     user_id: user
@@ -17,44 +119,44 @@ router.get("/new", (req, res) => {
   res.render("urls_new", templateVars);
 });
 
-router.get("/:id", (req, res) => {
+router.get("/urls/:id", (req, res) => {
   const user_id = req.cookies.user_id;
-  const user = req.app.get('users')[user_id];
+  const user = users[user_id];
 
   let templateVars = {
     shortURL: req.params.id,
-    longURL: req.app.get('urls')[req.params.id],
+    longURL: urlDatabase[req.params.id],
     user_id: user };
   res.render("urls_show", templateVars);
 });
 
-router.get("/", (req, res) => {
+router.get("/urls/", (req, res) => {
   const user_id = req.cookies.user_id;
-  const user = req.app.get('users')[user_id];
+  const user = users[user_id];
 
   let templateVars = {
-    urls: req.app.get('urls'),
+    urls: urlDatabase,
     user_id: user
   };
   res.render("urls_index", templateVars);
 });
 
-router.post("/:id/delete", (req, res) => {
-  delete req.app.get('urls')[req.params.id];
+router.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id];
   res.redirect('/urls');
 });
 
-router.post("/:id", (req, res) => {
-  req.app.get('urls')[req.params.id] = req.body.editURL;
+router.post("/urls/:id", (req, res) => {
+  urlDatabase[req.params.id] = req.body.editURL;
   res.redirect(`/urls/${req.params.id}`);
 });
 
 
-router.post("/", (req, res) => {
+router.post("/urls/", (req, res) => {
   const shortURL = generateRandomString();
   const longURL = req.body.longURL;
 
-  req.app.get('urls')[shortURL] = longURL;
+  urlDatabase[shortURL] = longURL;
   res.redirect(`/urls/${shortURL}`);
 });
 

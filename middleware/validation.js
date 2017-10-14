12025baf227ea0aebcee8a4cd7ca929701032685
eddycheong <cheong.e49@gmail.com>
2017-10-14 02:00:45 +0000

    Adding validation.js

---
+++ middleware/validation.js
@@ -0,0 +1,30 @@
+function userAuthentication(req, res, next) {
+  const userID = req.session.userID;
+  const urlOwner = req.url.userID;
+
+  if(userID !== urlOwner) {
+    res.status(403);
+    res.send("You do not have permissions to view this URL because you are not the owner.");
+    return;
+  }
+  next();
+}
+
+function validateShortUrl(req, res, next, shortURL) {
+  const shortUrlExist = req.db.urls.data[shortURL];
+
+  if(shortUrlExist) {
+    req.url = req.db.urls.data[shortURL];
+
+    next();
+    return;
+  }
+
+  res.status(404);
+  res.send(`Could not find the short URL: ${shortURL}`);
+}
+
+module.exports = {
+  userAuthentication: userAuthentication,
+  validateShortUrl: validateShortUrl
+};
